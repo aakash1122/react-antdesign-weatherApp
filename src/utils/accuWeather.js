08d2305fd6af0c6283507dca40213566e1a930dc
aakash1122 <aakaashhossain@gmail.com>
2019-10-17 00:23:28 +0600

    moved url into package.json file

--- src/utils/accuWeather.js
+++ src/utils/accuWeather.js
@@ -1,25 +1,10 @@
 import axios from "axios";
 
 export const apiKey = "9e4abad239d860aabb5db77e39b26ab1";
-export const url = `https://api.darksky.net/forecast/${apiKey}`;
+// export const url = `https://api.darksky.net/forecast/${apiKey}`;
 
 export const getCurrentWeatherDataByGeo = (lat, lon) => {
-  return axios.get(`${url}/${lat},${lon}`, {
+  return axios.get(`/${apiKey}/${lat},${lon}`, {
     params: { units: "si", exclude: "minutely,hourly" }
   });
 };
-
-// export const getLocationKeyByGeo = async (lat, lon) => {
-//   try {
-//     let { key } = await axios.get(
-//       "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search",
-//       {
-//         params: {
-//           apikey: apiKey,
-//           q: `${lat}, ${lon}`
-//         }
-//       }
-//     );
-//     console.log(key);
-//   } catch (error) {}
-// };
