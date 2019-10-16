dad519463e7bc68fb535be5b97d57764dc5fd51c
aakash1122 <aakaashhossain@gmail.com>
2019-10-17 01:45:47 +0600

    v2

--- src/utils/accuWeather.js
+++ src/utils/accuWeather.js
@@ -4,7 +4,7 @@ export const apiKey = "9e4abad239d860aabb5db77e39b26ab1";
 export const url = `https://api.darksky.net/forecast/${apiKey}`;
 
 export const getCurrentWeatherDataByGeo = (lat, lon) => {
-  return axios.get(`${url}/${lat},${lon}`, {
+  return axios.get(`https://cors-anywhere.herokuapp.com/${url}/${lat},${lon}`, {
     params: { units: "si", exclude: "minutely,hourly" }
   });
 };
