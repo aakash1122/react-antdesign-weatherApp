0bbb628c42269e437cee11b8cbfed0f6010f02a6
aakash1122 <aakaashhossain@gmail.com>
2019-10-19 01:50:03 +0600

    Updates

---
+++ precache-manifest.3987a08d735f29dc54c50879ebb5569f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae25cdaeb13b93a6be661301882b410f",
+    "url": "/react-antdesign-weatherApp/index.html"
+  },
+  {
+    "revision": "7683840e69a21c67cede",
+    "url": "/react-antdesign-weatherApp/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "976c6a1ebd0e8afb01b8",
+    "url": "/react-antdesign-weatherApp/static/css/main.b2ae0447.chunk.css"
+  },
+  {
+    "revision": "7683840e69a21c67cede",
+    "url": "/react-antdesign-weatherApp/static/js/2.eebfb0b3.chunk.js"
+  },
+  {
+    "revision": "976c6a1ebd0e8afb01b8",
+    "url": "/react-antdesign-weatherApp/static/js/main.2ff8469e.chunk.js"
+  },
+  {
+    "revision": "2ac45f5b9da0a6579289",
+    "url": "/react-antdesign-weatherApp/static/js/runtime-main.60872bf1.js"
+  },
+  {
+    "revision": "e95d705705409410d8d5117391120342",
+    "url": "/react-antdesign-weatherApp/static/media/sunrise.e95d7057.jpg"
+  },
+  {
+    "revision": "397c20e39c84117fb28501a9db90fb2d",
+    "url": "/react-antdesign-weatherApp/static/media/sunset.397c20e3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-antdesign-weatherapp/precache-manifest.acc75826fb5e8e3916e1a0490dd70eb0.js"
+  "/react-antdesign-weatherApp/precache-manifest.3987a08d735f29dc54c50879ebb5569f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-antdesign-weatherapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-antdesign-weatherApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
